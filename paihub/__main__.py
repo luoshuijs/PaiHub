1918eb6dab2600adf51acd478e2ca9093f7fb909
洛水居室 <luoshuijs@outlook.com>
2024-01-24 10:05:45 +0800

    :heavy_plus_sign: Add PaiHub Main Module

---
+++ paihub/__main__.py
@@ -0,0 +1,16 @@
+import asyncio
+
+from paihub.application import Application
+from paihub.log import logger
+
+try:
+    import uvloop
+
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+except ImportError:
+    uvloop = None
+
+
+application = Application()
+logger.info("PaiGram")
+application.run()
