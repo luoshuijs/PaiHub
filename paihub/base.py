7955ee4a7b8d16300730202f0b93de4b24a26976
洛水居室 <luoshuijs@outlook.com>
2024-01-24 10:04:29 +0800

    :heavy_plus_sign: Add Base Component

---
+++ paihub/base.py
@@ -0,0 +1,53 @@
+from typing import TYPE_CHECKING
+from persica import BaseComponent
+
+if TYPE_CHECKING:
+    from telegram.ext import Application as BotApplication
+    from paihub.application import Application
+
+
+class Component(BaseComponent, component=False):
+    pass
+
+
+class BaseDependence(BaseComponent, component=False):
+    async def initialize(self) -> None:
+        """Initialize resources used by this dependence"""
+
+    async def shutdown(self) -> None:
+        """Stop & clear resources used by this dependence"""
+
+
+class BaseRepository(BaseComponent, component=False):
+    pass
+
+
+class BaseService(BaseComponent, component=False):
+    application: "Application"
+
+    def set_application(self, application: "Application"):
+        self.application = application
+
+    async def initialize(self) -> None:
+        """Initialize resources used by this service"""
+
+    async def shutdown(self) -> None:
+        """Stop & clear resources used by this service"""
+
+
+class BaseCommand(BaseComponent, component=False):
+    application: "Application"
+    bot: "BotApplication"
+
+    def set_application(self, application: "Application"):
+        self.application = application
+        self.bot = application.bot
+
+    def add_handlers(self):
+        """Add bot handlers used by this function"""
+
+    async def initialize(self) -> None:
+        """Initialize resources used by this service"""
+
+    async def shutdown(self) -> None:
+        """Stop & clear resources used by this service"""
