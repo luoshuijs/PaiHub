d052d8823ef58edd78f8eed94fea7bf56c5fccbd
洛水居室 <luoshuijs@outlook.com>
2024-01-24 10:33:57 +0800

    :recycle: Add User Service

---
+++ paihub/system/user/cache.py
@@ -0,0 +1,25 @@
+from typing import List
+
+from paihub.base import Component
+from paihub.dependence.redis import Redis
+
+
+class UserAdminCache(Component):
+    def __init__(self, redis: Redis):
+        self.client = redis.client
+        self.qname = "users:admin"
+
+    async def ismember(self, user_id: int) -> bool:
+        return await self.client.sismember(self.qname, user_id)
+
+    async def get_all(self) -> List[int]:
+        return [int(str_data) for str_data in await self.client.smembers(self.qname)]
+
+    async def set(self, user_id: int) -> bool:
+        return await self.client.sadd(self.qname, user_id)
+
+    async def remove(self, user_id: int) -> bool:
+        return await self.client.srem(self.qname, user_id)
+
+    async def remove_all(self) -> bool:
+        return await self.client.delete(self.qname)
---
+++ paihub/system/user/repositories.py
@@ -0,0 +1,44 @@
+from typing import Optional, List
+
+from sqlmodel import select
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from paihub.base import Component
+from paihub.dependence.database import DataBase
+from paihub.system.user.entities import User
+
+__all__ = ("UserRepository",)
+
+
+class UserRepository(Component):
+    def __init__(self, database: DataBase):
+        self.engine = database.engine
+
+    async def get_by_user_id(self, user_id: int) -> Optional[User]:
+        async with AsyncSession(self.engine) as session:
+            statement = select(User).where(User.user_id == user_id)
+            results = await session.exec(statement)
+            return results.first()
+
+    async def add(self, user: User):
+        async with AsyncSession(self.engine) as session:
+            session.add(user)
+            await session.commit()
+
+    async def update(self, user: User) -> User:
+        async with AsyncSession(self.engine) as session:
+            session.add(user)
+            await session.commit()
+            await session.refresh(user)
+            return user
+
+    async def remove(self, user: User):
+        async with AsyncSession(self.engine) as session:
+            await session.delete(user)
+            await session.commit()
+
+    async def get_all(self) -> List[User]:
+        async with AsyncSession(self.engine) as session:
+            statement = select(User)
+            results = await session.exec(statement)
+            return results.all()
