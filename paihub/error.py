057d960c630dbddf430108b1af1eb36a4fde8a15
洛水居室 <luoshuijs@outlook.com>
2024-01-29 18:04:32 +0800

    :sparkles: Add `BadRequest`

--- paihub/error.py
+++ paihub/error.py
@@ -4,3 +4,10 @@ class PaiHubException(Exception):
 
 class PaiHubSystemExit(PaiHubException):
     pass
+
+
+class BadRequest(PaiHubException):
+    message: str
+
+    def __init__(self, message):
+        self.message = message
