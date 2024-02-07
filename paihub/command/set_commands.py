a840229f9050df08c9be117a31b9319c91e8fb2d
LuoShui <luoshuijs@outlook.com>
2024-02-07 10:37:36 +0800

    :sparkles: Add Set Bot Command

---
+++ paihub/command/set_commands.py
@@ -0,0 +1,47 @@
+from typing import TYPE_CHECKING
+
+from telegram import BotCommand, BotCommandScopeChat
+from telegram.ext import CommandHandler
+
+from paihub.base import BaseCommand
+from paihub.bot.handlers.adminhandler import AdminHandler
+from paihub.log import logger
+
+if TYPE_CHECKING:
+    from telegram import Update
+    from telegram.ext import ContextTypes
+
+
+class SetBotCommand(BaseCommand):
+    user = [
+        BotCommand("start", "Start！"),
+    ]
+
+    admin = [
+        BotCommand("review", "开始审核"),
+        BotCommand("push", "开始推送"),
+        BotCommand("reset", "重设审核"),
+        BotCommand("update", "更新代码"),
+        BotCommand("ping", "Ping！"),
+    ]
+
+    def add_handlers(self):
+        self.bot.add_handler(
+            AdminHandler(CommandHandler("set_admin_command", self.set_command, block=False), self.application)
+        )
+        self.bot.add_handler(
+            AdminHandler(CommandHandler("start", self.start_set, block=False), self.application), group=10
+        )
+
+    async def set_command(self, update: "Update", context: "ContextTypes.DEFAULT_TYPE"):
+        user = update.effective_user
+        chat = update.effective_chat
+        message = update.effective_message
+        logger.info("用户 %s[%s] 发出 set_command 命令", user.full_name, user.id)
+
+        await context.bot.set_my_commands(commands=self.user + self.admin, scope=BotCommandScopeChat(chat.id))
+        await message.reply_text("设置命令成功")
+
+    async def start_set(self, update: "Update", context: "ContextTypes.DEFAULT_TYPE"):
+        chat = update.effective_chat
+        await context.bot.set_my_commands(commands=self.user + self.admin, scope=BotCommandScopeChat(chat.id))
