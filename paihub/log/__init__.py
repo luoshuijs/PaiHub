c394214ab2625be6c53b97d1af995fa87b05e464
LuoShui <luoshuijs@outlook.com>
2024-02-08 15:54:29 +0800

    :recycle: Refactor Logger

--- paihub/log/__init__.py
+++ paihub/log/__init__.py
@@ -1,6 +1,6 @@
 from .logger import Logger
 
-__all__ = ("logger",)
+__all__ = ("logger", "Logger")
 
 
-logger = Logger()
+logger = Logger(name="PaiHub", filename="log.log", color_log=True)
--- paihub/log/logger.py
+++ paihub/log/logger.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import logging
 import os
 from logging.handlers import RotatingFileHandler
@@ -6,58 +7,50 @@ import colorlog
 
 
 class Logger:
-    _instance = None  # Singleton instance
-
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(Logger, cls).__new__(cls)
-            cls._instance.__initialized = False
-        return cls._instance
-
-    def __init__(self):
-        if self.__initialized:
-            return
-        self.__initialized = True
-
+    def __init__(
+        self,
+        name: Optional[str] = None,
+        level: int = logging.INFO,
+        filename: Optional[str] = None,
+        color_log: Optional[bool] = None,
+    ):
         log_path = os.path.join(os.getcwd(), "logs")
         if not os.path.exists(log_path):
             os.mkdir(log_path)
 
-        self.log_file_name = os.path.join(log_path, "log.log")
-        self.logger = logging.getLogger("PaiHub")
-        self.logger.setLevel(logging.INFO)  # Set the base level to DEBUG
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(level=level)
 
-        log_colors_config = {
-            "DEBUG": "cyan",
-            "INFO": "green",
-            "WARNING": "yellow",
-            "ERROR": "red",
-            "CRITICAL": "red",
-        }
+        if color_log:
+            log_colors_config = {
+                "DEBUG": "cyan",
+                "INFO": "green",
+                "WARNING": "yellow",
+                "ERROR": "red",
+                "CRITICAL": "red",
+            }
 
-        formatter = colorlog.ColoredFormatter(
-            "%(log_color)s[%(asctime)s] [%(levelname)s] - %(message)s",
-            log_colors=log_colors_config,
-        )
-        formatter_plain = logging.Formatter("[%(asctime)s] [%(levelname)s] - %(message)s")
+            formatter = colorlog.ColoredFormatter(
+                "%(log_color)s[%(asctime)s] [%(levelname)s] - %(message)s",
+                log_colors=log_colors_config,
+            )
+            # Console Handler
+            ch = colorlog.StreamHandler()
+            ch.setFormatter(formatter)
+            self.logger.addHandler(ch)
 
-        # Console Handler
-        ch = colorlog.StreamHandler()
-        # ch.setLevel(logging.INFO)  # Set to INFO or any other level
-        ch.setFormatter(formatter)
-
-        # File Handler
-        fh = RotatingFileHandler(
-            filename=self.log_file_name,
-            maxBytes=1024 * 1024 * 5,
-            backupCount=5,
-            encoding="utf-8",
-        )
-        # fh.setLevel(logging.INFO)  # Set to INFO or any other level
-        fh.setFormatter(formatter_plain)
-
-        self.logger.addHandler(fh)
-        self.logger.addHandler(ch)
+        if filename is not None:
+            # File Handler
+            log_file_name = os.path.join(log_path, filename)
+            fh = RotatingFileHandler(
+                filename=log_file_name,
+                maxBytes=1024 * 1024 * 5,
+                backupCount=5,
+                encoding="utf-8",
+            )
+            formatter_plain = logging.Formatter("[%(asctime)s] [%(levelname)s] - %(message)s")
+            fh.setFormatter(formatter_plain)
+            self.logger.addHandler(fh)
 
     def debug(self, message, *args, exc_info: any = None, **kwargs):
         self.logger.debug(message, *args, exc_info=exc_info, extra=kwargs)
