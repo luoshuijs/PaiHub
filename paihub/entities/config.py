5c17917bcffd29a8dacc896bcc9d231e1f750f94
LuoShui <luoshuijs@outlook.com>
2024-02-20 09:13:35 +0800

    :sparkles: Add `TomlConfig`

---
+++ paihub/entities/config.py
@@ -0,0 +1,16 @@
+import toml
+
+
+class TomlConfig(dict):
+    def __init__(self, config_path: str):
+        super().__init__()
+        self._config_path = config_path
+        self.read_toml_config()
+
+    def read_toml_config(self):
+        with open(self._config_path, "r") as file:
+            self.update(toml.load(file))
+
+    def write_toml_config(self):
+        with open(self._config_path, "w") as file:
+            toml.dump(self, file)
